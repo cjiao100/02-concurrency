2892d5863ae0bdf23e19c3350a58b7ae79a01ca9
cjiao100 <1971416012@qq.com>
2025-03-30 23:26:50 +0800

    feat: 更新Metrics模块，使用DashMap替代RwLock和HashMap以提高并发性能

--- src/metrics.rs
+++ src/metrics.rs
@@ -1,51 +1,45 @@
 // metrics data structure
 // 基础功能：inc/dec/snapshot
-use anyhow::{anyhow, Result};
+use anyhow::Result;
+use dashmap::DashMap;
 use std::{
-    collections::HashMap,
+    // collections::HashMap,
     fmt,
-    sync::{Arc, RwLock},
+    sync::Arc,
 };
 
 #[derive(Debug, Clone, Default)]
 pub struct Metrics {
-    data: Arc<RwLock<HashMap<String, i64>>>,
+    data: Arc<DashMap<String, i64>>, // Arc<RwLock<HashMap<String, i64>>> => Arc<DashMap<String, i64>>
 }
 
 impl Metrics {
     pub fn new() -> Self {
         Metrics {
-            data: Arc::new(RwLock::new(HashMap::new())),
+            data: Arc::new(DashMap::new()),
         }
     }
 
     pub fn inc(&self, key: impl Into<String>) -> Result<()> {
-        let mut data = self.data.write().map_err(|e| anyhow!(e.to_string()))?;
-        let counter = data.entry(key.into()).or_insert(0);
+        let mut counter = self.data.entry(key.into()).or_insert(0);
         *counter += 1;
 
         Ok(())
     }
 
     pub fn dec(&self, key: impl Into<String>) -> Result<()> {
-        let mut data = self.data.write().map_err(|e| anyhow!(e.to_string()))?;
-        let counter = data.entry(key.into()).or_insert(0);
+        let mut counter = self.data.entry(key.into()).or_insert(0);
         *counter -= 1;
 
         Ok(())
-    }
-
-    pub fn snapshot(&self) -> Result<HashMap<String, i64>> {
-        let data = self.data.read().map_err(|e| anyhow!(e.to_string()))?;
-        Ok(data.clone())
     }
 }
 
 impl fmt::Display for Metrics {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        let data = self.data.read().map_err(|_e| fmt::Error)?;
-        for (key, value) in data.iter() {
-            writeln!(f, "{}: {}", key, value)?;
+        // let data = self.data.read().map_err(|_e| fmt::Error)?;
+        for entry in self.data.iter() {
+            writeln!(f, "{}: {}", entry.key(), entry.value())?;
         }
 
         Ok(())
