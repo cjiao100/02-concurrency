933903d82d602f0d65b734768e607702081b222b
cjiao100 <1971416012@qq.com>
2025-03-30 21:24:11 +0800

    feat: 添加指标模块，支持计数和快照功能

---
+++ examples/metrics.rs
@@ -0,0 +1,58 @@
+use std::{thread, time::Duration};
+
+use anyhow::Result;
+use concurrency::Metrics;
+use rand::Rng;
+
+const N: usize = 2;
+const M: usize = 4;
+fn main() -> Result<()> {
+    let metrics = Metrics::new();
+
+    // start n workers and m requesters
+
+    for i in 0..N {
+        task_worker(i, metrics.clone())?; // Metrics{ data: Arc::clone(&metrics.data) }
+    }
+
+    for _ in 0..M {
+        request_worker(metrics.clone())?;
+    }
+
+    loop {
+        thread::sleep(Duration::from_secs(1));
+        let snapshot = metrics.snapshot();
+        println!("{:?}", snapshot);
+    }
+
+    #[allow(unreachable_code)]
+    Ok(())
+}
+
+fn task_worker(idx: usize, metrics: Metrics) -> Result<()> {
+    thread::spawn(move || {
+        loop {
+            let mut rng = rand::rng();
+            thread::sleep(Duration::from_millis(rng.random_range(100..5000)));
+            metrics.inc(format!("call.thread.worker.{}", idx))?;
+        }
+        #[allow(unreachable_code)]
+        Ok::<_, anyhow::Error>(())
+    });
+
+    Ok(())
+}
+
+fn request_worker(metrics: Metrics) -> Result<()> {
+    thread::spawn(move || {
+        loop {
+            let mut rng = rand::rng();
+            thread::sleep(Duration::from_millis(rng.random_range(50..800)));
+            let page = rng.random_range(1..256);
+            metrics.inc(format!("req.page.{}", page))?;
+        }
+        #[allow(unreachable_code)]
+        Ok::<_, anyhow::Error>(())
+    });
+    Ok(())
+}
